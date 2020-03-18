8d3939185b586a5c287ccc00cc8fa6aad1df0bd6
Ibnu Hajar <ibnuhajardewantara62@gmail.com>
2020-03-18 16:34:35 +0700

    memberi menu di sidebar

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,8 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('template/header');
+		$data['judul']='Home';
+		$this->load->view('template/header',$data);
 		$this->load->view('home/index');
 		$this->load->view('template/footer');
 	}
