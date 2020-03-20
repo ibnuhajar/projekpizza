1cee74e97e44b5e8cdc5cdc1ae97c4b7d11ff9c1
Ibnu Hajar <ibnuhajardewantara62@gmail.com>
2020-03-20 14:33:26 +0700

    pengaturan database

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -2,7 +2,46 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+		  <div class="jumbotron">
+			<h1 class="display-4">Welcome </h1>
+			<table class="table table-success">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">NO</th>
+      <th scope="col">Nip</th>
+      <th scope="col">Nama</th>
+	  <th scope="col">Gaji</th>
+	  <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+	  <?php $i=1; ?>
+ <?php foreach ($a as $a):?>
+	<tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+
+ <?php endforeach ?> 
+  
+    
+  </tbody>
+</table>
+		</div>
 
         </div>
         <!-- /.container-fluid -->
@@ -11,43 +50,3 @@
       <!-- End of Main Content -->
 
 
-
-      <!-- Footer -->
-      <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2020</span>
-          </div>
-        </div>
-      </footer>
-      <!-- End of Footer -->
-
-    </div>
-    <!-- End of Content Wrapper -->
-
-  </div>
-  <!-- End of Page Wrapper -->
-
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-  </a>
-
-  <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
-        </div>
-      </div>
-    </div>
-  </div>
