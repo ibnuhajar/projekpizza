bdeb737a6f968722297dee5ef22ff7ba1892281c
Ibnu Hajar <ibnuhajardewantara62@gmail.com>
2020-03-29 20:03:33 +0700

    lupa udah sampek mana jadi nya gk ada komit

--- application/views/slip/index.php
+++ application/views/slip/index.php
@@ -1,28 +1,175 @@
 	<!-- Begin Page Content -->
 	<div class="container-fluid">
-	<h1 class="display-4">Welcome </h1>
-	<!-- Page Heading -->
-	<div class="jumbotron bg-white">
+		<!-- section insert -->
+		<div class="d-sm-flex align-items-center justify-content-between mb-4">
+			<h1 class="h3 mb-0 text-gray-800">Welcome</h1>
 
-	<!-- section insert -->
+			<!-- Button trigger modal -->
+            <button type="button" class="d-none d-sm-inline-block btn btn-sm btn-success btn-icon-split shadow-sm" data-toggle="modal" data-target="#exampleModalCenter">
+				<span class="icon text-white-50">
+					<i class="fas fa-plus"></i>
+				</span>
+				<span class="text">
+					Add Item
+				</span>
+			</button>
+		</div>
+		
 	
-	<a href="<?=base_url(); ?>/Slip/insert" class="badge badge-success">
-	Add Item
-	</a>
 
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+		<div class="modal-header">
+			<h5 class="modal-title" id="exampleModalCenterTitle">Form Data Kariawan</h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<di class="modal-body">
 
-		<table class="table table-bordered">
-		<thead class="thead-dark">
-			<tr>
-			<th scope="col">NO</th>
-			<th scope="col">Nip</th>
-			<th scope="col">Nama</th>
-			<th scope="col">Gaji</th>
-			<th scope="col">Action</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $i=1; ?>
+		<!-- section form modal -->
+	<form action="" method="post">
+	<div class="form-row">
+		<div class="form-group col-md-6">
+			<label name="no" for="no">No</label>
+			<input type="text" class="form-control" name="no" id="no" placeholder="Nomer">
+		</div>
+		
+		<div class="form-group col-md-6">
+			<label name="bagian" for="bagian">Bagian</label>
+				<select name="bagian" id="bagian" class="form-control">
+					<option selected>Superfasior</option>
+					<option>Manager</option>
+					<option>Marketing</option>
+					<option>Sales</option>
+					<option>Analys</option>
+					<option>Programer</option>
+				</select>
+		</div>
+	</div>
+
+	<div class="form-row">
+		<div class="form-group col-md-6">
+			<label name="nip" for="nip">Nip</label>
+			<input type="text" class="form-control" name="nip" id="nip" placeholder="Nip">
+		</div>
+		<div class="form-group col-md-6">
+			<label name="golongan" for="golongan">Golongan</label>
+				<select name="golongan" id="golongan" class="form-control">
+					<option selected>B1</option>
+					<option>B2r</option>
+					<option>B3</option>
+					<option>B4</option>
+					<option>B5</option>
+					<option>B6</option>
+				</select>
+		</div>
+	</div>
+
+	<div class="form-row">
+		<div class="form-group col-md-6">
+		<label name="nama" for="nama">Nama</label>
+		<input type="text" class="form-control" name="nama" id="nama" placeholder="Nama">
+		</div>
+		<div class="form-group col-md-6">
+		<label name="uang" for="uang">Rp.</label>
+		<input type="text" class="form-control" name="uang" id="uang" placeholder="Rp.">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="gaji" name="gaji">Gaji untuk Bulanan</label>
+		<input type="text" class="form-control" name="gaji" id="gaji" placeholder="Rp. ">
+	</div>
+	
+	<!-- section potongan -->
+		<h5 class="text-center">Potongan Potongan</h5>
+	<hr class="sidebar-divider my-0">
+	<div class="form-group mt-1">
+		<label for="iuran" name="iuran">Iuran Kopri</label>
+		<input type="text" class="form-control" name="iuran" id="iuran" placeholder="Rp. ">
+	</div>
+	<div class="form-group">
+		<label for="simpan" name="simpan">Simpanan Koperasi / Bantuan Kemalangan</label>
+		<input type="text" class="form-control" name="simpan" id="simpan" placeholder="Rp. ">
+	</div>
+	<div class="form-group">
+		<label for="darma" name="darma">Dharma Wanita</label>
+		<input type="text" class="form-control" name="darma" id="darma" placeholder="Rp. ">
+	</div>
+	<div class="form-group">
+		<label for="asuransi" name="asuransi">Asuransi</label>
+		<input type="text" class="form-control" name="asuransi" id="asuransi" placeholder="Rp. ">
+	</div>
+	<div class="form-group">
+		<label for="bank" name="bank">Bank Ke</label>
+		<input type="text" class="form-control" name="bank" id="bank" placeholder="Nomor ">
+	</div>
+	<div class="form-group">
+		<label for="kpn" name="kpn">KPN Ke</label>
+		<input type="text" class="form-control" name="kpn" id="kpn" placeholder="Nomor ">
+	</div>
+	<div class="form-group">
+		<label for="potong" name="potong">Jumlah Potongan</label>
+		<input type="text" class="form-control" name="potong" id="potong" placeholder="Rp. ">
+	</div>
+	<hr class="sidebar-divider my-2">
+
+	<!-- sevtion keseluruhan -->
+	<div class="form-group">
+		<label for="seluruh" name="seluruh">Jumlah Penerimaan Bersih</label>
+		<input type="text" class="form-control" name="seluruh" id="seluruh" placeholder="Nomor ">
+	</div>
+
+	<div class="form-group mr-sm-2 pr-2 mb-2">
+    <label for="digital" name="digital">Digital Signatur</label>
+    <input type="text" class="form-control" name="digatal" id="digital" placeholder="xxxxxxxxxxxxxxxxxxxxxxxxxx">
+  </div>
+  <button type="submit" class="btn btn-success mb-1">Enkripsi</button>
+</form>
+	
+
+<!-- end form modal -->
+		<div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+			<a href="<?=base_url(); ?>" type="button" class="btn btn-success">Save</a>
+		</div>
+		</div>
+	</div>
+	</div>
+
+	
+	<div class="container-fluid">
+
+			<!-- DataTales -->
+			<div class="card shadow mb-4">
+            <div class="card-header py-3">
+            	<h6 class="m-0 font-weight-bold text-primary">Data Tables Karyawan</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    	<tr>
+							<th>No</th>
+							<th>Nip</th>
+							<th>Nama</th>
+							<th>Gaji</th>
+							<th>Action</th>	
+                    	</tr>
+                	</thead>
+                	<tfoot>
+                    	<tr>
+							<th>No</th>
+							<th>Nip</th>
+							<th>Nama</th>
+							<th>Gaji</th>
+							<th>Action</th>
+                		</tr>
+                  </tfoot>
+                  <tbody>
+				  <?php $i=1; ?>
 		<?php foreach ($karyawan as $k):?>
 			<tr>
 			<th scope="row"><?= $i; ?></th>
@@ -35,13 +182,15 @@
 			</td>
 			</tr>
 			<?php $i++; ?>
-		<?php endforeach; ?> 
-		</tbody>
-		</table>
+		<?php endforeach; ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
 
-	</div>
-	</div>
-	<!-- /.container-fluid -->
+        </div>
+        <!-- /.container-fluid -->
 
 </div>
 <!-- End of Main Content -->
--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -48,7 +48,14 @@
   <script src="<?=base_url('vendor/');  ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="<?=base_url('vendor/');  ?>js/sb-admin-2.min.js"></script>
+	<script src="<?=base_url('vendor/');  ?>js/sb-admin-2.min.js"></script>
+	
+	<!-- Page level plugins -->
+  <script src="<?=base_url('vendor/');  ?>vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="<?=base_url('vendor/');  ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="<?=base_url('vendor/');  ?>js/demo/datatables-demo.js"></script>
 
 </body>
 
